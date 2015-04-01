f5e47ddd53e876218e8542c230ce1424c8651da4
Ruan Pienaar <ruanpienaar@users.noreply.github.com>
2015-04-01 16:23:36 +0100

    Update crell_server.erl

--- apps/crell/src/crell_server.erl
+++ apps/crell/src/crell_server.erl
@@ -35,7 +35,7 @@ calc_proc(Pid,Opts) ->
 
 init({}) ->
     Node = application:get_env(crell, node),
-    true = net_kernel:connect(Ndoe),
+    true = net_kernel:connect(Node),
     {ok,AppMonPid} = rpc:call(Node,crell_appmon,start_appmon,[]),
     {ok, #?STATE{appmon_pid = AppMonPid,
                  node = Node}}.
