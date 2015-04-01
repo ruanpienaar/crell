471bed76fddd9ea3a15c4bf1e81c6a7eecdd2dfe
Ruan Pienaar <ruanpienaar@users.noreply.github.com>
2015-04-01 16:57:54 +0100

    Update crell_server.erl

--- apps/crell/src/crell_server.erl
+++ apps/crell/src/crell_server.erl
@@ -5,7 +5,7 @@
          calc_proc/1,
          calc_proc/2
         ]).
--export([remote_which_applications/0])
+-export([remote_which_applications/0]).
 
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
