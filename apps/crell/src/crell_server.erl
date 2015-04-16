2cc7d23176d2f76c42ef20c0ba16957dcc2ecbca
ruanpienaar <ruan800@gmail.com>
2015-04-16 16:08:20 +0100

    Add set cookie for remote node

--- apps/crell/src/crell_server.erl
+++ apps/crell/src/crell_server.erl
@@ -14,7 +14,8 @@
 -define(SERVER, ?MODULE).
 -define(STATE, crell_server_state).
 -record(?STATE, { appmon_pid,
-                  node
+                  remote_node,
+                  remote_node_cookie
                 }).
 
 start_link() ->
@@ -41,22 +42,30 @@ remote_which_applications() ->
 %% ---------------------------------------
 
 init({}) ->
-    {ok,Node} = application:get_env(crell, node),
-    true = net_kernel:connect(Node),
-%     {ok,AppMonPid} = 
-    rpc:call(Node,crell_appmon,start_appmon,[]),
-    {ok, #?STATE{node = Node}}.
+    {ok,Node} = application:get_env(crell, remote_node),
+    {ok,Cookie} = application:get_env(crell, remote_cookie),
+    case erlang:set_cookie(Node,Cookie) of 
+    	true ->
+    		true = net_kernel:connect(Node),
+		    rpc:call(Node,crell_appmon,start_appmon,[]),
+		    {ok, #?STATE{
+		    	remote_node = Node,
+		    	remote_node_cookie = Cookie
+		    }};
+		false ->
+			{stop, {enoconnect,[{node,Node},{cookie,Cookie}]}}
+	end.
 
-handle_call({app,App,Opts}, _From, #?STATE{ node = Node } = State) ->
+handle_call({app,App,Opts}, _From, #?STATE{ remote_node = Node } = State) ->
     R = rpc:call(Node,crell_appmon,calc_app_tree,[App, Opts]),
     {reply, R, State};
-handle_call({pid,Pid,Opts}, _From, #?STATE{ node = Node } = State) ->
+handle_call({pid,Pid,Opts}, _From, #?STATE{ remote_node = Node } = State) ->
     R = rpc:call(Node,crell_appmon,calc_proc_tree,[Pid, Opts]),
     {reply, R, State};
-handle_call({app_env,AppName}, _From, #?STATE{ node = Node } = State) ->
+handle_call({app_env,AppName}, _From, #?STATE{ remote_node = Node } = State) ->
     AppEnv = rpc:call(Node, application, get_all_env, [AppName]),
     {reply, {ok,AppEnv}, State};
-handle_call(remote_which_applications, _From, #?STATE{ node = Node } = State) ->
+handle_call(remote_which_applications, _From, #?STATE{ remote_node = Node } = State) ->
    Apps = rpc:call(Node, application, which_applications, []),
    {reply,Apps,State};
 handle_call(_Request, _From, State) ->
