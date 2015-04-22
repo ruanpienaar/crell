6cad60fe1ba5ad3258f5e7767d20ebd667c70249
ruanpienaar <ruan800@gmail.com>
2015-04-22 15:06:58 +0100

    fix code browser rest handler

--- apps/crell/src/crell_app_env.erl
+++ apps/crell/src/crell_app_env.erl
@@ -4,17 +4,21 @@
           terminate/3,
           content_types_provided/2 %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
 ]).
+-export([ handle_json/2
+]).
 
 -define(STATE,crell_app_env_state).
 -record(?STATE,{app_name}).
 
 init(Req, Opts) ->
-    {cowboy_rest, Req, Opts}.
+	AppName = cowboy_req:binding(app_name,Req),
+    {cowboy_rest, Req, #?STATE{ app_name = list_to_atom(binary_to_list(AppName)) }}.
 
 content_types_provided(Req, State) ->
-	io:format("State : ~p\n",[State]),
-    AppName = cowboy_req:binding(app_name,Req),
-    {[{<<"application/json">>, handle_json}], Req, #?STATE{ app_name = list_to_atom(binary_to_list(AppName)) }}.
+	
+	{[{<<"application/json">>, handle_json}], Req, State}.
+
+    %% {[{<<"application/json">>, handle_json}], Req, State}.
 
 handle_json(Req, State) ->
     {ok,Data} = crell_server:calc_app_env(State#?STATE.app_name),
