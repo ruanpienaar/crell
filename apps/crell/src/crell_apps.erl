15bb6f35d0da9fe0db4f38f15f82641fb163dfa1
Ruan Pienaar <ruanpienaar@users.noreply.github.com>
2015-04-22 14:10:26 +0100

    Update crell_apps.erl

--- apps/crell/src/crell_apps.erl
+++ apps/crell/src/crell_apps.erl
@@ -1,72 +1,32 @@
 -module(crell_apps).
 
 -export([ init/2,
-          rest_init/2,
-          terminate/3 ]).
+          terminate/3,
+          content_types_provided/2 %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
+]).
+
+-export([ handle_json/2
+        ]).
 
 -define(STATE,crell_apps_state).
 -record(?STATE,{}).
 
-%% All available REST handler exports:
- -export([
-%     allowed_methods/2        %% Methods : all
-%     allow_missing_post/2     %% Methods : POST
-%     charsets_provided/2      %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     content_types_accepted/2 %% Methods : POST, PUT, PATCH
-    content_types_provided/2 %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     delete_completed/2       %% Methods : DELETE
-%     delete_resource/2        %% Methods : DELETE
-%     expires/2                %% Methods : GET, HEAD
-%     forbidden/2              %% Methods : all
-%     generate_etag/2          %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     is_authorized/2          %% Methods : all
-%     is_conflict/2            %% Methods : PUT
-%     known_methods/2          %% Methods : all
-%     languages_provided/2     %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     last_modified/2          %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     malformed_request/2      %% Methods : all
-%     moved_permanently/2      %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     moved_temporarily/2      %% Methods : GET, HEAD, POST, PATCH, DELETE
-%     multiple_choices/2       %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     options/2                %% Methods : OPTIONS
-%     previously_existed/2     %% Methods : GET, HEAD, POST, PATCH, DELETE
-%     resource_exists/2        %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
-%     service_available/2      %% Methods : all
-%     uri_too_long/2           %% Methods : all
-%     valid_content_headers/2  %% Methods : all
-%     valid_entity_length/2    %% Methods : all
-%     variances/2              %% Methods : GET, HEAD, POST, PUT, PATCH, DELETE
- ]).
-
--export([
-         handle_json/2
-        ]).
-
-init(_Req, []) ->
-    {upgrade, protocol, cowboy_rest}.
-
-rest_init(Req, _Opts) ->
-    {ok, Req, #?STATE{ } }.
+init(Req, Opts) ->
+    {cowboy_rest, Req, Opts}.
 
 content_types_provided(Req, State) ->
     {[{<<"application/json">>, handle_json}], Req, State}.
 
 handle_json(Req, State) ->
-    % [{stdlib,"ERTS  CXC 138 10","2.3"},
-    %  {kernel,"ERTS  CXC 138 10","3.1"}]
-%     AppList = application:which_applications(),
     AppList = crell_server:remote_which_applications(),
     Json = to_json(AppList, []),
     {Json,Req,State}.
 
 terminate(normal, _Req, _State) ->
-    io:format("Terminate! ~p\n",[normal]),
     ok;
 terminate({crash, Class, Reason}, _Req, _State) ->
-    io:format("Terminate! ~p\n",[{crash, Class, Reason}]),
     ok;
 terminate(Reason, _Req, _State) ->
-    io:format("Terminate! ~p\n",[Reason]),
     ok.
 
 to_json([],R) ->
