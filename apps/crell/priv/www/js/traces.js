59d8e899d67607ef03c06962bf6f83d6bf4b37fd
ruanpienaar <ruan800@gmail.com>
2016-11-08 15:47:20 +0000

    Prettifying the UI

--- apps/crell/priv/www/js/traces.js
+++ apps/crell/priv/www/js/traces.js
@@ -5,3 +5,221 @@
 //   });
 
 // })();
+
+
+
+
+
+
+    function message(string){
+        console.log(string);
+    }
+
+    // <pre><code class="html">...</code></pre>
+
+    function process_trace_obj(obj){
+
+        var table_row='';
+        switch (obj['type']) {
+            case 'trace':
+                table_row=
+                '<tr>'+
+                ' <th scope="row">'+obj['datetime']+'</th>'+
+                ' <td>'+obj['pid']+'</td>'+
+                ' <td>'+obj['label']+'</td>'+
+                ' <td><pre><code class="erlang">'+obj['info']+'</code></pre></td>'+
+                '</tr>';
+                $('#traces_table').append(table_row);
+                break;
+            case 'trace_extra':
+                table_row=
+                '<tr>'+
+                ' <th scope="row">'+obj['datetime']+'</th>'+
+                ' <td>'+obj['pid']+'</td>'+
+                ' <td>'+obj['label']+'</td>'+
+                ' <td><pre><code class="erlang">'+obj['info']+'</code></pre></td>'+
+                ' <td><pre><code class="erlang">'+obj['extra']+'</code></pre></td>'+
+                '</tr>';
+                $('#traces_table').append(table_row);
+                break;
+            case 'drop':
+                console.log('Dropped '+obj['dropped']+' trace msgs.');
+                break;
+        }
+    }
+
+    function connect(){
+        try{
+            var socket;
+            var url = window.location.href;
+            var arr = url.split("/"); 
+            var host = "ws://"+arr[2]+"/goanna_api/ws";
+            var socket = new WebSocket(host);
+
+            socket.onopen = function(){
+                //message('onopen : '+socket.readyState+' (open)');
+                socket.send(JSON.stringify({'get':'nodes'}));
+                socket.send(JSON.stringify({'get':'active_traces'}));
+                socket.send(JSON.stringify({'get':'runtime_modules'}));
+            }
+
+            socket.onmessage = function(msg){
+                if(msg.data=='ok'){
+                    // why did we return ok anyways???...
+                }else{
+                    var json_data = JSON.parse(msg.data);
+
+                    if(json_data.hasOwnProperty('traces')) {
+                        for(var i = 0; i < json_data['traces'].length; i++) {
+                            var obj = json_data['traces'][i];
+                            process_trace_obj(obj);
+                        }
+                    } else if(json_data.hasOwnProperty('nodes')) {
+                        $('#nodes').empty();
+                        for(var i = 0; i < json_data['nodes'].length; i++) {
+                            var obj = json_data['nodes'][i];
+                            $('#nodes').append('<option value='+obj['node']+' >'+obj['node']+'</option>')
+                        }
+                    } else if(json_data.hasOwnProperty('active_traces')) {
+                        $('#trace_patterns').empty();
+                        for(var i = 0; i < json_data['active_traces'].length; i++) {
+                            var obj = json_data['active_traces'][i];
+                            var mfastr = obj['module']+':'+obj['function']+'/'+obj['arity'];
+                            $('#trace_patterns').append('<option value='+mfastr+'>'+mfastr+'</option>');
+                        }
+                    } else if(json_data.hasOwnProperty('runtime_modules')) {
+                        $('#trace_mod').empty();
+                        $('#trace_mod').append('<option value="*" >*</option>');
+                        for(var i = 0; i < json_data['runtime_modules'].length; i++) {
+                            var obj = json_data['runtime_modules'][i];
+                            $('#trace_mod').append('<option value='+obj+' >'+obj+'</option>')
+                        }
+                    } else if(json_data.hasOwnProperty('functions')) {
+                        $('#trace_fun').empty();
+                        $('#trace_fun').append('<option value="*" >*</option>');
+                        for(var i = 0; i < json_data['functions'].length; i++) {
+                            var obj = json_data['functions'][i];
+                            $('#trace_fun').append('<option value='+obj+' >'+obj+'</option>')
+                        }
+                    } else if(json_data.hasOwnProperty('traces')) {
+                        for(var i = 0; i < json_data['traces'].length; i++) {
+                            var obj = json_data['traces'][i];
+                            
+                        }
+                    } else if(json_data.hasOwnProperty('traces_polling_end')) {
+                        $('#pollBtn').attr("disabled", false);
+                        $('#stoppollBtn').attr("disabled", true);
+                        $('#loaderImg').attr("class", "invisible");
+                    }
+                }
+            }
+
+            socket.onclose = function(){
+                //message('onclose : '+socket.readyState+' (Closed)');
+                // Try and reconnect...
+                // maybe create a sleep....
+                // connect();
+            }
+
+            $('#pollBtn').click(function(){
+                $('#pollBtn').attr("disabled", true);
+                $('#stoppollBtn').attr("disabled", false);
+                $('#loaderImg').attr("class", "visible");
+                socket.send( JSON.stringify({"polling":"true"}) );
+            });
+
+            $('#stoppollBtn').click(function(){
+                socket.send( JSON.stringify({"polling":"false"}) );
+            });
+
+            $('#fetchBtn').click(function(evt){
+                socket.send( JSON.stringify( {"fetch":$('#fetchSelect').val()} ));
+            });
+
+            $('#traceBtn').click(function(evt){
+                var trace_json = {
+                    'mod':$('#trace_mod').val(),
+                    'fun':$('#trace_fun').val(),
+                    'ara':$('#trace_ara').val(),
+                    'tim':$('#trace_tim').val(),
+                    'mes':$('#trace_mes').val()
+                };
+                socket.send( JSON.stringify( {'trace': trace_json } ));
+            });
+
+            $('#disconnectBtn').click(function(){
+                var ans=confirm('Do you want to disconnect '+$('#nodes option:selected').val());
+                if(ans){
+                    socket.send(JSON.stringify({'module': 'goanna_api',
+                                                'function': 'remove_node',
+                                                'args':
+                                                    [$('#nodes option:selected').val()]
+                                                    //'['+$('#nodes option:selected').val()+']'
+                                               })
+                    );
+                }
+            });
+
+            $('#addNodeBtn').click(function(){
+                socket.send(JSON.stringify({'module':'goanna_api',
+                                            'function':'add_node',
+                                            'args':
+                                                [$('#add_node').val(),
+                                                 $('#add_cookie').val(),
+                                                 $('#add_type').val()]
+                                           })
+                );
+            });
+
+            $('#trace_mod').change(function(){
+                socket.send(JSON.stringify({'module':'crell_server',
+                                            'function':'runtime_module_functions',
+                                            'args':
+                                                [$('#trace_mod').val()]
+                                           })
+                );
+            });
+
+            $('#stopTracePatBtn').click(function(){
+
+                socket.send(JSON.stringify({'module':'goanna_api',
+                                            'function':'stop_trace',
+                                            'args':
+                                                [$('#trace_patterns').val()]
+                                           })
+                );
+            });
+            
+            $('#stopTraceBtn').click(function(){
+                socket.send( JSON.stringify({"polling":"false"}) );
+                socket.send(JSON.stringify({'module':'goanna_api',
+                                            'function':'stop_trace',
+                                            'args':
+                                                []
+                                           })
+                );
+            });
+
+        } catch(exception) {
+            message('!!! EXCEPTION: '+exception);
+        }
+    }
+
+    // on document ready, connect the ws
+    $(document).ready(function() {
+        if(!("WebSocket" in window)){
+            $('<p>Oh no, you need a browser that supports WebSockets. How about <a href="http://www.google.com/chrome">Google Chrome</a>?</p>').appendTo('#container');
+        }else{
+           //The user has WebSockets
+           connect();
+        }
+
+        $( "#clearBtn" ).click(function() {
+            $('#traces_table').empty();
+        });
+
+        $('#printBtn').click(function(){
+            window.print();
+        });
+
+    });
--- apps/crell/src/crell_goanna_api_ws.erl
+++ apps/crell/src/crell_goanna_api_ws.erl
@@ -6,72 +6,57 @@
 -export([websocket_handle/3]).
 -export([websocket_info/3]).
 
+-define(STATE, crell_goanna_api_ws).
+-record(?STATE, { polling = false }).
+
 
 init(Req, Opts) ->
     erlang:register(crell_goanna_api_ws, self()),
     process_flag(trap_exit, true),
-
-    {cowboy_websocket, Req, Opts}.
-
-websocket_handle({text, <<"fetch">>}, Req, State) ->
-    % fetcher(25),
-    {reply, {text, <<"ok">>}, Req, State};
-% websocket_handle({text, Msg}, Req, State) ->
-%     {reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
+    % io:format("Opts : ~p~n", [Opts]),
+    {cowboy_websocket, Req, #?STATE{}}.
 
 websocket_handle({text, ReqJson}, Req, State) ->
+    % io:format("State : ~p~n", [State]),
     case jsx:decode(ReqJson) of
         [{<<"get">>, <<"nodes">>}] ->
             Json = get_nodes_json(),
             {reply, {text, Json}, Req, State};
+
         [{<<"get">>, <<"active_traces">>}] ->
-            ActiveTracesFull = goanna_api:list_active_traces(),
-            ActiveTraces =
-                lists:foldl(fun({{Node,TrcPattern},Now,TrcOpts}, Acc) ->
-                    case lists:keyfind(TrcPattern, 1, Acc) of
-                        false ->
-                            [{TrcPattern,
-                              crell_web_utils:ens_bin(TrcPattern#trc_pattern.m),
-                              crell_web_utils:ens_bin(undef_function(TrcPattern#trc_pattern.f)),
-                              crell_web_utils:ens_bin(undef_arity(TrcPattern#trc_pattern.a))
-                            }|Acc];
-                        {TrcPattern,_,_,_} ->
-                            Acc
-                    end
-                end, [], ActiveTracesFull),
-            Json  = jsx:encode(
-                [{<<"active_traces">>,
-                    [ [
-                        {<<"module">>,   M},
-                        {<<"function">>, F},
-                        {<<"arity">>,    A}
-                      ] || {_, M,F,A} <- ActiveTraces ]
-                }]
-            ),
+        Json = active_traces_json(),
             {reply, {text, Json}, Req, State};
+
         [{<<"get">>, <<"runtime_modules">>}] ->
             JsonRunMods = jsx:encode( [ {<<"runtime_modules">>,
                                         [ crell_web_utils:ens_bin(Mod) || Mod <- crell_server:runtime_modules() ]}
                                       ] ),
             {reply, {text, JsonRunMods}, Req, State};
-        [{<<"module_functions">>}] ->
-            {reply, {text, <<"">>}, Req, State};
 
         [{<<"trace">>, Details}] ->
             ok = trace(Details),
-            {reply, {text, <<"ok">>}, Req, State};
+            Json = active_traces_json(),
+            {reply, {text, Json}, Req, State};
+
+        %% Use a countdown timer to only poll, for a while..
+        [{<<"polling">>,<<"true">>}] ->
+            poller(100, 100),
+            {reply, {text, <<"ok">>}, Req, State#?STATE{ polling = true }};
+
+        [{<<"polling">>,<<"false">>}] ->
+            %% stop_poller(),
+            {reply, {text, <<"ok">>}, Req, State#?STATE{ polling = false }};
 
         [{<<"fetch">>,_SizeBin}] ->
             % _SizeInt = list_to_integer(binary_to_list(SizeBin)),
-            Traces = goanna_api:pull_all_traces(),
-            Json = jsx:encode( [{<<"traces">>, [ dbg_trace_format_to_json(T) || T <- Traces ]}] ),
+            Json = all_traces_json(),
             {reply, {text, Json}, Req, State};
 
         [{<<"module">>,<<"goanna_api">>},
          {<<"function">>,<<"remove_node">>},
          {<<"args">>,[Node]}] ->
             %% TODO: how will i handle errors?
-            goanna_api:remove_node(list_to_atom(binary_to_list(Node))),
+            ok = goanna_api:remove_node(list_to_atom(binary_to_list(Node))),
             Json = get_nodes_json(),
             {reply, {text, Json}, Req, State};
 
@@ -79,45 +64,72 @@ websocket_handle({text, ReqJson}, Req, State) ->
          {<<"function">>,<<"add_node">>},
          {<<"args">>,[Node, Cookie, Type]}] ->
             %% TODO: how will i handle errors?
-            goanna_api:add_node(list_to_atom(binary_to_list(Node)),
+             {ok, _GoannaNodePid} = 
+                  goanna_api:add_node(list_to_atom(binary_to_list(Node)),
                                       list_to_atom(binary_to_list(Cookie)),
                                       list_to_atom(binary_to_list(Type))
                                      ),
             Json = get_nodes_json(),
             {reply, {text, Json}, Req, State};
+
         [{<<"module">>,<<"crell_server">>},
          {<<"function">>,<<"runtime_module_functions">>},
          {<<"args">>,[Mod]}] ->
+            %% TODO: maybe list * as an arity, so the front end user, can select, all arity.
             Functions = crell_server:runtime_module_functions(list_to_atom(binary_to_list(Mod))),
             Json = jsx:encode([{<<"functions">>, [ crell_web_utils:ens_bin(atom_to_list(F)++"/"++integer_to_list(Arity))
                 || {F,Arity} <- Functions ]}]),
             {reply, {text, Json}, Req, State};
-        A ->
-            io:format("~p~n", [A]),
-            {reply, {text, <<"ok">>}, Req, State}
+
+        [{<<"module">>,<<"goanna_api">>},
+         {<<"function">>,<<"stop_trace">>},
+         {<<"args">>,[]}] ->
+            io:format("Here 1~n", []),
+            ok = goanna_api:stop_trace(),
+            Json = active_traces_json(),
+            {reply, {text, Json}, Req, State};
+
+        [{<<"module">>,<<"goanna_api">>},
+         {<<"function">>,<<"stop_trace">>},
+         {<<"args">>,[TrcPattern]}] ->
+            [M, F, A] = trcpat_mfa_to_str(TrcPattern),
+            %% TODO: handle * as all arity, so call, stop_trace(M,F)
+            io:format("Here 2~n", []),
+            ok = goanna_api:stop_trace(M, F, A),
+            Json = active_traces_json(),
+            {reply, {text, Json}, Req, State};
+
+        UnknownJson ->
+            io:format("UnknownJson: ~p~n", [UnknownJson]),
+            Json = jsx:encode([{<<"unknown_json">>, UnknownJson}]),
+            {reply, {text, Json}, Req, State}
     end;
 websocket_handle(Data, Req, State) ->
-    io:format("Data : ~p\n\n", [Data]),
+    io:format("websocket_handle : ~p\n\n", [Data]),
     {ok, Req, State}.
 
-
-websocket_info({timeout, _Ref, <<"fetch">>}, Req, State) ->
-    case goanna_api:pull_all_traces() of
-        [] ->
-            fetcher(1000);
-        Traces ->
-            fetcher(25)
-    end,
-    {reply, {text, <<"Traces...">>}, Req, State};
+websocket_info({timeout, _Ref, {<<"fetch">>, Count}}, Req, #?STATE{ polling = false } = State) ->
+    Json = polling_end_json(),
+    {reply, {text, Json}, Req, State};
+websocket_info({timeout, _Ref, {<<"fetch">>, Count}}, Req, #?STATE{ polling = true } = State) ->
+    NewCount = Count - 1,
+    Json = 
+        case NewCount of
+            0 ->
+                polling_end_json();
+            _ ->
+                poller(500, Count-1),
+                all_traces_json()
+        end,
+    {reply, {text, Json}, Req, State};
 websocket_info(Info, Req, State) ->
     io:format("~p~n", [Info]),
     {ok, Req, State}.
 
-fetcher() ->
-    fetcher(1000).
-
-fetcher(Ms) ->
-    erlang:start_timer(Ms, self(), <<"fetch">>).
+poller(Ms, Count) when Count =< 0 ->
+    ok;
+poller(Ms, Count) when Count > 0 ->
+    erlang:start_timer(Ms, self(), {<<"fetch">>, Count}).
 
 ensure_info(T) ->
     crell_web_utils:ens_bin(io_lib:format("~p", [T])).
@@ -131,7 +143,6 @@ dbg_trace_format_to_json({Now, {trace, Pid, Label, Info}}) ->
      {<<"pid">>, crell_web_utils:ens_bin(Pid)},
      {<<"label">>, crell_web_utils:ens_bin(Label)},
      {<<"info">>, ensure_info(Info)}];
-
 dbg_trace_format_to_json({Now, {trace, Pid, Label, Info, Extra}}) ->
     [{<<"datetime">>, crell_web_utils:ens_bin(crell_web_utils:localtime_ms_str(Now))},
      {<<"type">>, crell_web_utils:ens_bin(trace_extra)},
@@ -139,7 +150,6 @@ dbg_trace_format_to_json({Now, {trace, Pid, Label, Info, Extra}}) ->
      {<<"label">>, crell_web_utils:ens_bin(Label)},
      {<<"info">>, ensure_info(Info)},
      {<<"extra">>, ensure_extra(Extra)}];
-
 dbg_trace_format_to_json({Now, {drop, NumDropped}}) ->
     [{<<"datetime">>, crell_web_utils:ens_bin(crell_web_utils:localtime_ms_str(Now))},
      {<<"type">>, crell_web_utils:ens_bin(drop)},
@@ -150,8 +160,8 @@ trace([{<<"mod">>,Mod},
     goanna_api:trace(crell_web_utils:ens_atom(Mod));
 trace([{<<"mod">>,Mod},
        {<<"fun">>,Fun}]) ->
-    [Func, Ara] = string:tokens(binary_to_list(Fun), "/"),
-    goanna_api:trace(crell_web_utils:ens_atom(Mod), crell_web_utils:ens_atom(Func), crell_web_utils:ens_int(Ara)).
+    [F,A] = trcpat_fa_to_str(Fun),
+    goanna_api:trace(crell_web_utils:ens_atom(Mod), F, A).
 %%trace([{<<"mod">>,Mod},
 %%       {<<"fun">>,Fun},
 %%       {<<"ara">>,Arity},
@@ -174,6 +184,13 @@ proplist_to_json_ready(Props) ->
         [KVPair]
     end, Props).
 
+trcpat_mfa_to_str(Mfa) ->
+    [M, F, A] = string:tokens(binary_to_list(Mfa), ":/"),
+    [crell_web_utils:ens_atom(M), crell_web_utils:ens_atom(F), crell_web_utils:ens_int(A)].
+
+trcpat_fa_to_str(Fa)->
+    [F, A] = string:tokens(binary_to_list(Fa), "/"),
+    [crell_web_utils:ens_atom(F), crell_web_utils:ens_int(A)].
 
 get_nodes_json() ->
     Nodes = goanna_api:nodes(),
@@ -184,3 +201,35 @@ get_nodes_json() ->
                {<<"type">>, crell_web_utils:ens_bin(Type)}] || {Node,Cookie,Type} <- Nodes ]
         }]
     ).
+
+active_traces_json() ->
+    ActiveTracesFull = goanna_api:list_active_traces(),
+            ActiveTraces =
+                lists:foldl(fun({{Node,TrcPattern},Now,TrcOpts}, Acc) ->
+                    case lists:keyfind(TrcPattern, 1, Acc) of
+                        false ->
+                            [{TrcPattern,
+                              crell_web_utils:ens_bin(TrcPattern#trc_pattern.m),
+                              crell_web_utils:ens_bin(undef_function(TrcPattern#trc_pattern.f)),
+                              crell_web_utils:ens_bin(undef_arity(TrcPattern#trc_pattern.a))
+                            }|Acc];
+                        {TrcPattern,_,_,_} ->
+                            Acc
+                    end
+                end, [], ActiveTracesFull),
+            jsx:encode(
+                [{<<"active_traces">>,
+                    [ [
+                        {<<"module">>,   M},
+                        {<<"function">>, F},
+                        {<<"arity">>,    A}
+                      ] || {_, M,F,A} <- ActiveTraces ]
+                }]
+            ).
+
+    all_traces_json() ->
+        Traces = goanna_api:pull_all_traces(),
+        jsx:encode( [{<<"traces">>, [ dbg_trace_format_to_json(T) || T <- Traces ]}] ).
+
+    polling_end_json() ->
+        jsx:encode([{<<"traces_polling_end">>, <<"true">>}]).
